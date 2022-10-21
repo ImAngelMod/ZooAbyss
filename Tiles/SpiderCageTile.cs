8207a0bece3c38f0eb2c1acb10b77befb702d8cd
ImAngel <113800751+ImAngelMod@users.noreply.github.com>
2022-10-21 11:24:03 +0200

    Add files via upload

---
+++ Tiles/SpiderCageTile.cs
@@ -0,0 +1,48 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using ZooAbis.Items;
+
+namespace ZooAbis.Tiles
+{
+    public class SpiderCageTile : ModTile
+
+    {
+        public override void SetStaticDefaults()
+
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLighted[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileID.Sets.DisableSmartCursor[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true; 
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style6x3);
+            TileObjectData.newTile.Width = 6;
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16 }; //
+
+            AnimationFrameHeight = 54;
+            TileObjectData.addTile(Type);
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(new Terraria.DataStructures.EntitySource_TileBreak(i, j), i * 16, j * 16, 48, 24, ModContent.ItemType<SpiderCage>());
+        }
+
+        public override void AnimateTile(ref int frame, ref int frameCounter)
+        {
+            frameCounter++;
+            if (frameCounter > 8) 
+            {
+                frameCounter = 0;
+                frame++;
+                if (frame > 4)
+                {
+                    frame = 0;
+                }
+            }
+        }
+    }
+}
